2b270e626a92fcfb5388ea25c5bbc2847c428d63
ayoub <ayoubafdel@gmail.com>
2022-03-13 18:08:09 +0100

    announce

--- cpp-01/ex01/main.cpp
+++ cpp-01/ex01/main.cpp
@@ -4,5 +4,9 @@
 int main()
 {
     Zombie *z = zombieHorde(5, "ayoub");
+    for (int i = 0; i < 5; i++){
+        z[i].announce();
+    }
+    std::cout << "costructor call:\n";
     delete[] z;
 }
