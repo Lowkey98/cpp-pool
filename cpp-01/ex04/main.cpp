b11930ecf52667683b4e4127d9fb968e09de71e8
ayoub <ayoubafdel@gmail.com>
2022-03-31 18:01:20 +0000

    main fix dispersion

--- cpp-01/ex04/main.cpp
+++ cpp-01/ex04/main.cpp
@@ -1,25 +1,41 @@
+#include <iostream>
+#include <fstream>
 
-#include "HumanA.hpp"
-#include "HumanB.hpp"
-int main()
+int main(int argc, char const *argv[])
 {
+    if (argc != 4)
     {
-        Weapon club = Weapon("crude spiked club");
-
-        HumanA bob("Bob", club);
-        bob.attack();
-        club.setType("some other type of club");
-        bob.attack();
+        std::cerr << "need 3 arguments" << std::endl;
+        return (1);
     }
+    std::ifstream InputFile;
+    std::ofstream OutputFile;
+    std::string line;
+
+    std::string OutputName = (std::string)argv[1] + ".replace";
+    InputFile.open(argv[1], std::ios::in);
+    OutputFile.open(OutputName.c_str(), std::ios::out);
+    while (getline(InputFile, line))
     {
-        Weapon club = Weapon("crude spiked club");
-
-        HumanB jim("Jim");
-        jim.setWeapon(club);
-        jim.attack();
-        club.setType("some other type of club");
-        jim.attack();
+        for(size_t i = 0; i < line.length(); i++)
+        {
+            int index = line.substr(i, line.length()).find(argv[2]);
+            if (index == -1){
+                OutputFile << line.substr(i, line.length() - i);
+                break;
+            }
+            else{
+                OutputFile << line.substr(i, index);
+                OutputFile << argv[3];
+            }
+            i = i + index + ((std::string)argv[2]).length() - 1;
+        }
+        if (InputFile.peek() == EOF) {
+            break;
+        }       
+        OutputFile << std::endl;
     }
-    // std::cout << << std::endl;
-
+    InputFile.close();
+    OutputFile.close();
+    return 0;
 }
