83189beba646d4bfe44ab5a0b2f1bfa65e8054ba
ayoub <ayoubafdel@gmail.com>
2022-03-31 22:36:24 +0000

    tested main idk shallow test

--- cpp-04/ex01/main.cpp
+++ cpp-04/ex01/main.cpp
@@ -4,10 +4,14 @@
 
 int main()
 {
-    const Animal* j = new Dog();
-    const Animal* i = new Cat();
-    delete j;//should not create a leak
-    delete i;
+    Animal *animal_array[2];
+    animal_array[0] = new Dog("dog");   
+    animal_array[1] = new Cat("cat");
+    for (int i = 0; i < 2; i++)
+    {
+        animal_array[i]->makeSound();
+        delete animal_array[i];
+    }
     return 0;
 }
 
