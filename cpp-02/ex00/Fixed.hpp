c62e40f5206c989d4fd4abe9a65651aac57ff9fa
Ayoub Afdel <ayafdel@e1r11p10.1337.ma>
2022-04-02 17:39:20 +0000

    added setRawbits

--- cpp-02/ex00/Fixed.hpp
+++ cpp-02/ex00/Fixed.hpp
@@ -1,8 +1,12 @@
+#if !defined(FIXED_HPP)
+#define FIXED_HPP
+
 # include <string>
 # include <iostream>
-class Fixed{
+class Fixed
+{
     private: 
-        int _fixed_n;
+        int _fixed;
         static const int n_fraction = 8;
     public:
         Fixed();
@@ -12,3 +16,5 @@ class Fixed{
         int getRawBits( void ) const;
         void setRawbits( int const raw);
 };
+
+#endif // FIXED_HPP
