7ef70df16091e4da05bc9470ae955c241fcff907
ayoub <ayoubafdel@gmail.com>
2022-04-23 01:10:20 +0000

    second implementation of own iterator

--- cpp-08/ex02/MutantStack.hpp
+++ cpp-08/ex02/MutantStack.hpp
@@ -1,51 +1,62 @@
 #include <stack>
 #include <iostream>
+#include<list>
 
-template<typename T>
-class MyIterator
-{
-    public:
-        T* ptr;
-    T   &operator*()
-    {
-        return *this->ptr; 
-    }
-    T   &operator++()
-    {
-        this->ptr++;
-        return (this->ptr);
-    }
-    T   *operator++(int)
-    {
-        T *tmp = this->ptr;
-        this->ptr++;
-        return (tmp);
-    }
-};
 
-//  cant assign a reference
-template<typename T>
-bool operator!=(MyIterator<T> lhs, MyIterator<T> rhs)
-{
-    return lhs.ptr != rhs.ptr;
-}
 
 template<typename T>
 class MutantStack : public std::stack<T>
 {
-    private:
-        MyIterator<T> it;
-    public:
-        MyIterator<T>  begin()
+public:
+    class iterator
+    {
+        public:
+            T* ptr;
+        T   &operator*()
         {
-            MyIterator<T> it;
+            return *this->ptr; 
+        }
+        T   &operator++()
+        {
+            this->ptr++;
+            return (*this->ptr);
+        }
+        T   &operator--()
+        {
+            this->ptr--;
+            return (*this->ptr);
+        }
+        T   *operator++(int)
+        {
+            T *tmp = this->ptr;
+            this->ptr++;
+            return (tmp);
+        }
+        T   *operator--(int)
+        {
+            T *tmp = this->ptr;
+            this->ptr--;
+            return (tmp);
+        }
+        bool operator!=(MutantStack::iterator rhs)
+        {
+            return this->ptr != rhs.ptr;
+        }
+    };
+        MutantStack<T>::iterator  begin()
+        {
+            MutantStack<T>::iterator it;
             it.ptr = &this->top() - this->size() + 1;
             return it; 
         }
-        MyIterator<T>  end()
+        MutantStack<T>::iterator  end()
         {
-            MyIterator<T> it;
+            MutantStack<T>::iterator it;
             it.ptr = &this->top() + 1;
             return it; 
         }
+        MutantStack<T>::iterator it;
 };
+
+//  cant assign a reference
+
--- cpp-08/ex02/main.cpp
+++ cpp-08/ex02/main.cpp
@@ -1,15 +1,41 @@
 #include "MutantStack.hpp"
+// int main()
+// {
+//     MutantStack <int> a;
+//     a.push(1);
+//     a.push(2);
+//     a.push(3);
+//     a.push(4);
+//     a.push(5);
+//     a.push(6);
+//     for (MutantStack<int>::MyIterator  it = a.begin(); it != a.end(); it++)
+//     {
+//         std::cout << *it << std::endl;
+//     }
+
+// }
 int main()
 {
-    MutantStack <int> a;
-    a.push(1);
-    a.push(2);
-    a.push(3);
-    a.push(4);
-    a.push(5);
-    a.push(6);
-    for (MyIterator <int> it = a.begin(); it != a.end(); it++)
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    //[...]
+    mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
     {
-        std::cout << *it << std::endl;
+    std::cout << *it << std::endl;
+    ++it;
     }
+    std::stack<int> s(mstack);
+    return 0;
 }
